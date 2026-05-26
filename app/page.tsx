2f9d7787a531d85f75f6da0ef6ed4b5f02efa02f
menukahansda <menukahansda199@gmail.com>
2026-05-26 23:43:11 +0530

    Add SpendForm to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
+import SpendForm from "@/components/forms/SpendForm";
 export default function Home() {
   return (
     <>
+      <SpendForm />
     </>
       
   );
 }
+
