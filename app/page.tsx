b23616d0333f5ccfd62f53b615eae78f27e6cddb
menukahansda <menukahansda199@gmail.com>
2026-05-30 13:50:58 +0530

    Connect SpendForm with audit hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import SpendForm from "@/components/forms/SpendForm";
+import useAudit from "@/hooks/useAudit";
+import type { AuditFormData } from '@/components/forms/SpendForm';
+
 export default function Home() {
+  const {submitAudit, loading, error, result} = useAudit();
+
   return (
     <>
-      <SpendForm />
+      {!result && !loading && <SpendForm handleSubmit={submitAudit} loading={loading}/>}
+      {loading && <p>Loading...</p>}
     </>
       
   );
 }
 
+// before result & loading => spendform
+// after loading => loading spinner
+// after result => audit result page or redirect to app/audit
