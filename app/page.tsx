c713c17c8f952b488536451652dd5a36676eec6b
menukahansda <menukahansda199@gmail.com>
2026-06-09 12:33:00 +0530

    Add router logic to navigate to audit page once result are available

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,24 @@
 
 import SpendForm from "@/components/forms/SpendForm";
 import useAudit from "@/hooks/useAudit";
-import type { AuditFormData} from "@/lib/audit/types";
+import { useRouter} from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const {submitAudit, loading, error, result} = useAudit();
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (result) {
+      router.push("/audit");
+    }
+  }, [result, router]);
 
   return (
     <>
       {!result && !loading && <SpendForm handleSubmit={submitAudit} loading={loading}/>}
       {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
     </>
       
   );
